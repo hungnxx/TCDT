feb230276fc720c6458c2f2a301d3a9dca674206
nguyenHung <nguyenxuanhung23011998@gmail.com>
2022-01-21 19:02:13 +0700

    aaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardUserComponent } from './board-user/board-user.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { RegisterComponent } from './containers/register/register.component';
+import { LoginComponent } from './containers/login/login.component';
+import { HomeComponent } from './containers/home/home.component';
+import { ProfileComponent } from './containers/profile/profile.component';
+import { BoardUserComponent } from './containers/board-user/board-user.component';
+import { BoardModeratorComponent } from './containers/board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './containers/board-admin/board-admin.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'user', component: BoardUserComponent },
   { path: 'mod', component: BoardModeratorComponent },
-  { path: 'admin', component: BoardAdminComponent },
+  // { path: 'admin', component: BoardAdminComponent },
+  { path: 'admin', component: LayoutComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
---
+++ src/app/containers/annual-estimat-and3-year-plan/annual-estimat-and3-year-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-annual-estimat-and3-year-plan',
+  templateUrl: './annual-estimat-and3-year-plan.component.html',
+  styleUrls: ['./annual-estimat-and3-year-plan.component.css']
+})
+export class AnnualEstimatAnd3YearPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
